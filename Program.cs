8e9524f0a5aa4c0660b90dc0c6aac0c0edc53711
R. G. Esteves <rgesteves@gmail.com>
2018-11-25 23:16:15 -0800

    Initial import

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jsonrpctest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
